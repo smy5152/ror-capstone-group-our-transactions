ba95470aea75b4f1eed3fe93add830b06f6cabb2
smy5152 <shabbiryamani@gmail.com>
2021-05-06 17:06:35 +0200

    rubocop: fixing indentation

--- spec/views/tasks/edit.html.erb_spec.rb
+++ spec/views/tasks/edit.html.erb_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "tasks/edit", type: :view do
+RSpec.describe 'tasks/edit', type: :view do
   before(:each) do
     @task = assign(:task, Task.create!(
-      name: "MyString",
-      amount: 1
-    ))
+                            name: 'MyString',
+                            amount: 1
+                          ))
   end
 
-  it "renders the edit task form" do
+  it 'renders the edit task form' do
     render
 
-    assert_select "form[action=?][method=?]", task_path(@task), "post" do
+    assert_select 'form[action=?][method=?]', task_path(@task), 'post' do
+      assert_select 'input[name=?]', 'task[name]'
 
-      assert_select "input[name=?]", "task[name]"
-
-      assert_select "input[name=?]", "task[amount]"
+      assert_select 'input[name=?]', 'task[amount]'
     end
   end
 end
