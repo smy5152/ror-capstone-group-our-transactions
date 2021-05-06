52b51c56444cb4e8166dcba6bc73a8d4876cca77
smy5152 <shabbiryamani@gmail.com>
2021-05-06 22:21:38 +0200

    MIGRATE: Relations, User, Group, Tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_011002) do
+ActiveRecord::Schema.define(version: 2021_05_06_201203) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2021_05_06_011002) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", default: 1, null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -51,6 +53,8 @@ ActiveRecord::Schema.define(version: 2021_05_06_011002) do
     t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id", default: 1, null: false
+    t.index ["author_id"], name: "index_tasks_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,4 +66,6 @@ ActiveRecord::Schema.define(version: 2021_05_06_011002) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "groups", "users"
+  add_foreign_key "tasks", "users", column: "author_id"
 end
