92c1d6f0a19e1dc011ac562afc3a398ba93aa61d
smy5152 <shabbiryamani@gmail.com>
2021-05-07 03:14:27 +0200

    Helpers,Layouts- render partials for routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def session_partials
+    if logged_in?
+      render partial: 'partials/signedin'
+    else
+      render partial: 'partials/notsignedin'
+    end
+  end
 end
