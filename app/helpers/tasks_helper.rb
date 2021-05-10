6f3c283c9555b92d9660871471d2d4d0a75435c6
smy5152 <shabbiryamani@gmail.com>
2021-05-10 03:11:56 +0200

    Task CARDS: Add css and bootstrap layout for 1 card, New Task, Group & Ungrouped Task

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,12 @@
 module TasksHelper
+  def display_icon(group, task)
+    return unless task.group
+
+    image_tag group.icon, alt: 'group-icon', class: 'group-img'
+  end
+
+  def total_tasks_hours(tasks)
+    total = tasks.inject(0) { |acum, task| acum + task.amount }
+    "#{total} Hours"
+  end
 end
